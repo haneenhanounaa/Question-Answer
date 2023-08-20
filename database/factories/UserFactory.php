663291f3cfb6116b56cb685a8a2980d85dd079a3
haneenhanounaa <haneenhanounaem@gmail.com>
2023-08-20 13:52:32 +0300

    some edtis

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,13 +3,16 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
+
 {
+    protected $model=User::class; //1
     /**
      * Define the model's default state.
      *
@@ -18,7 +21,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'name' => fake()->name(),// فاكر بترجع الاسم عشوائي
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
